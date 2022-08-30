8d6d2703727beacd35769d61e7ab229eefb0ea90
JpopAmrock <87099312+JpopAmrock@users.noreply.github.com>
2022-08-30 17:39:58 -0400

    sdf

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -4,7 +4,7 @@ import logo from '../../assets/logo.svg'
 import github from '../../assets/github.svg'
 import styles from './AppHeader.css' // eslint-disable-line
 
-const AppHeader = (props) => {
+const AppHeader = (_props) => {
   return (
     <header className='app-header'>
       <div className='app-title-wrapper'>
